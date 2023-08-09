be140f30ead60d38a4fe968dd329d03cc9984e76
Priyanshu Raj <116699775+priyyanshuraj@users.noreply.github.com>
2023-08-09 17:58:22 +0000

    Day 1 of 100 day of js

--- 100DaysofJS/test.js
+++ 100DaysofJS/test.js
@@ -1,1 +1,15 @@
-console.log("Hitesh");
+//**************  Operations ********************
+
+let value =3 
+let negValue = -value
+console.log(negValue);
+//Adding two strings
+let str1 ="Priyanshu "
+let str2 ="Raj"
+// console.log(str1 + str2);
+let str3 = str1+str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 +3);
+console.log(1 + 2 +"2");
